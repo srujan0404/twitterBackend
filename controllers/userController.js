72459f3bc323e7f6e8c141c4be05e8d716cf1bdc
srujan0404 <srujan.23bcs10107@ms.sst.scaler.com>
2024-03-27 23:30:58 +0530

    resolved one error

--- controllers/userController.js
+++ controllers/userController.js
@@ -62,7 +62,7 @@ export const Login = async (req, res) => {
             userId: user._id
         }
         const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET, { expiresIn: "1d" });
-        return res.status(201).cookie("token", token, { expiresIn: "1d", httpOnly: true }).json({
+        return res.status(201).cookie("token", token, { expiresIn: "1d" }).json({
             message: `Welcome back ${user.name}`,
             user,
             success: true
